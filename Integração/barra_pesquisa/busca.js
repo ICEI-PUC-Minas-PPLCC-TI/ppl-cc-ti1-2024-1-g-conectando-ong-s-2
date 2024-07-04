c291ac5b39dfcc04633fb272db53261aab51ac53
Giovana Fioravante <166036827+GiovanaFioravante@users.noreply.github.com>
2024-07-03 23:30:06 -0300

    Delete Integração directory

--- Integração/barra_pesquisa/busca.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('botao-filtro').addEventListener('click', function() {
-    var filtros = document.getElementById('filtros');
-    if (filtros.style.display === 'flex') {
-        filtros.style.display = 'none';
-    } else {
-        filtros.style.display = 'flex';
-    }
-});
